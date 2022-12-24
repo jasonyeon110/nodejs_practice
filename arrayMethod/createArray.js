39e0323a2d7a535ff469efe5850574adddfbb495
Jason Yeon <jasonyeon110@gmail.com>
2022-12-24 22:08:02 +0900

    just little update

---
+++ arrayMethod/createArray.js
@@ -0,0 +1,5 @@
+var names = ["rj", "ricky", "alex"];
+console.log(names);
+var arr = new Array(3);
+arr = [1,4,7];
+
---
+++ errorsInNode/newError.js
@@ -0,0 +1,3 @@
+const err = new Error("this is an Error Msg")
+console.log(err.message)
+console.log(err.code)
