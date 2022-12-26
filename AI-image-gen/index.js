ece7ea07b43e494863306548b0b4647a478e0f56
Jason Yeon <jasonyeon110@gmail.com>
2022-12-26 10:50:25 +0900

    OpenAi backend

--- AI-image-gen/index.js
+++ AI-image-gen/index.js
@@ -4,6 +4,10 @@ const port = process.env.PORT || 5500;
 
 const app = express();
 
+// Enable body parser
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 app.use('/openai', require('./routes/openaiRoutes'));
 
 app.listen(port, () => console.log(`Listening on ${port}`));
